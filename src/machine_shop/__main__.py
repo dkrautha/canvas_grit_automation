a1f3130013cbbbf4ea6e5449630f82a1873d07e9
David Krauthamer <dkrautha@pm.me>
2023-08-31 11:14:14 -0400

    Get user creation date from canvas

--- src/machine_shop/__main__.py
+++ src/machine_shop/__main__.py
@@ -24,7 +24,7 @@ def get_quiz_results(
     course: Course,
     quiz_name: str,
     quiz_id: int,
-    since_time: datetime,
+    since_time: datetime | None = None,
 ) -> pl.LazyFrame:
     assignment = course.get_assignment(quiz_id)
     submissions = assignment.get_submissions(start_time=since_time)
@@ -34,6 +34,7 @@ def get_quiz_results(
     cwids: list[str] = []
     scores: list[float] = []
     emails: list[str] = []
+    enrolled_year: list[datetime] = []
 
     for sub in submissions:
         student = course.get_user(sub.user_id)
@@ -45,12 +46,14 @@ def get_quiz_results(
             continue
         score = float(sub.grade.replace("%", ""))
         email: str = student.login_id
+        enrolled: datetime = student.created_at_date
 
         first_names.append(first_name)
         last_names.append(last_name)
         cwids.append(cwid)
         scores.append(score)
         emails.append(email)
+        enrolled_year.append(enrolled)
 
     is_passing = pl.col(quiz_name) >= 90
     return (
@@ -60,13 +63,15 @@ def get_quiz_results(
                 "lastName": last_names,
                 "externalId": cwids,
                 "email": emails,
+                "enrollment_date": enrolled_year,
                 quiz_name: scores,
             },
             {
-                "firstName": str,
-                "lastName": str,
-                "externalId": str,
-                "email": str,
+                "firstName": pl.Utf8,
+                "lastName": pl.Utf8,
+                "externalId": pl.Utf8,
+                "email": pl.Utf8,
+                "enrollment_date": pl.Datetime,
                 quiz_name: pl.Float32,
             },
         )
@@ -76,23 +81,14 @@ def get_quiz_results(
 
 
 def get_database() -> pl.LazyFrame:
-    return pl.read_excel(DATABASE_PATH).lazy()
+    return pl.scan_csv(DATABASE_PATH, dtypes={"externalId": pl.Utf8})
 
 
 def main():
     LAST_24_HOURS = datetime.now() - timedelta(hours=24)
 
-    # database = get_database()
-    # temporary, in theory grab the current list from grit?
-    database = pl.LazyFrame(
-        {
-            "firstName": ["Not"],
-            "lastName": ["Real"],
-            "externalId": ["8675309"],
-            "email": ["notreal@stevens.edu"],
-            **{quiz_name: [True] for quiz_name in QUIZ_IDS.keys()},
-        }
-    )
+    database = get_database()
+    updated_db = database
 
     canvas = Canvas(API_URL, API_KEY)
     course = canvas.get_course(COURSE_ID)
@@ -102,22 +98,31 @@ def main():
             course,
             quiz_name,
             quiz_id,
-            LAST_24_HOURS,
+            None,  # Change to LAST_24_HOURS in future
         )
 
-        database = upsert(
-            database, results, ["externalId", "email", "firstName", "lastName"]
+        updated_db = upsert(
+            updated_db, results, ["externalId", "email", "firstName", "lastName"]
         )
 
-    updated_db = database.with_columns(
+    database = database.collect()
+    database.write_csv(
+        f"{DATABASE_PATH.with_suffix('')}_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}{DATABASE_PATH.suffix}"
+    )
+
+    updated_db = updated_db.collect()
+    updated_db.write_csv(DATABASE_PATH)
+
+    give_to_grit = updated_db.with_columns(
         [
             pl.when(pl.col(c)).then(pl.lit("x")).otherwise(pl.lit(None)).alias(c)
             for c in QUIZ_IDS.keys()
         ]
-    ).collect()
-
-    updated_db.write_csv("grit_export.csv")
+    ).drop("enrollment_date")
+    give_to_grit.write_csv("give_to_grit.csv")
 
 
 if __name__ == "__main__":
-    main()
+    import cProfile
+
+    cProfile.run("main()", sort="tottime")
