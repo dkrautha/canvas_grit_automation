145ef7407af37dbeca4c8da014c11d9bc2394055
David Krauthamer <dkrautha@pm.me>
2023-09-13 11:31:24 -0400

    Simplify env loading functions

--- src/machine_shop/env.py
+++ src/machine_shop/env.py
@@ -1,15 +1,14 @@
 import os
 
+
+def get_env_or_raise(key: str) -> str:
+    if (env := os.getenv(key)) is None:
+        raise ValueError(f"{key} not set in environment")
+    return env
+
+
 def get_quiz_ids() -> dict[str, int]:
-    ENV = os.getenv("CANVAS_QUIZ_IDS")
-    if ENV is None:
-        raise ValueError("CANVAS_QUIZ_IDS not set")
-    PAIRS = ENV.split(",")
-    LIST_OF_PAIRS = [pair.split("=") for pair in PAIRS]
-    return {pair[0]: int(pair[1]) for pair in LIST_OF_PAIRS}
-
-
-def try_get_env(key: str) -> str:
-    if (ENV := os.getenv(key)) is None:
-        raise ValueError(f"{key} not set")
-    return ENV
+    env = get_env_or_raise("CANVAS_QUIZ_IDS")
+    pairs = env.split(",")
+    list_of_pairs = [pair.split("=") for pair in pairs]
+    return {pair[0]: int(pair[1]) for pair in list_of_pairs}
