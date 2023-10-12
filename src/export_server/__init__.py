b76d2c13838b64321eca7885c04114962fd1083c
David Krauthamer <dkrautha@pm.me>
2023-10-12 12:35:57 -0400

    Save timestamped version of the export from grit

--- src/export_server/__init__.py
+++ src/export_server/__init__.py
@@ -1,4 +1,6 @@
 import io
+from datetime import datetime
+from pathlib import Path
 
 import requests
 from dotenv import load_dotenv
@@ -26,4 +28,7 @@ def grit_export():
     )
     response = session.send(request)
     file = io.BytesIO(response.content)
+    timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M")
+    file_name = Path(f"grit_export_{timestamp}.xlsx")
+    file_name.write_bytes(file.getvalue())
     return send_file(file, mimetype="application/vnd.ms-excel")
