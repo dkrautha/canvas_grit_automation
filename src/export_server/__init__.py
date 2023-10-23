adebe48b8cdc1e782b14c34b8f6d79d5c091bb52
David Krauthamer <dkrautha@pm.me>
2023-10-23 15:05:17 -0400

    Properly convert from grit excel to csv

--- src/export_server/__init__.py
+++ src/export_server/__init__.py
@@ -15,6 +15,7 @@ app = Flask(__name__)
 load_dotenv()
 GRIT_URL = get_env_or_raise("GRIT_URL")
 GRIT_API_KEY = get_env_or_raise("GRIT_API_KEY")
+BACKUP_FOLDER = Path(get_env_or_raise("BACKUP_FOLDER"))
 
 
 @app.route("/grit_export", methods=["GET"])
@@ -29,9 +30,15 @@ def grit_export():
     )
     response = session.send(request)
     as_excel = io.BytesIO(response.content)
-    as_csv = io.BytesIO()
-    pl.read_excel(as_excel).write_csv(as_csv)
 
     timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M")
-    Path(f"grit_export_{timestamp}.csv").write_bytes(as_csv.getvalue())
-    return send_file(as_csv)
+    filepath = BACKUP_FOLDER / f"grit_export_{timestamp}.csv"
+    filepath.touch()
+    pl.read_excel(as_excel).write_csv(filepath)
+
+    return send_file(
+        filepath,
+        mimetype="text/csv",
+        as_attachment=True,
+        download_name="grit_export.csv",
+    )
