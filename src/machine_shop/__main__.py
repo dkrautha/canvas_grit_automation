99dc6f39b515bf33fe93504a886ec8a7fb0e5a68
David Krauthamer <dkrautha@pm.me>
2024-01-22 00:47:56 -0500

    Update main script to condense changes into one upload instead of four

--- src/machine_shop/__main__.py
+++ src/machine_shop/__main__.py
@@ -1,6 +1,7 @@
 import io
 import logging
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
+from pathlib import Path
 from typing import cast
 
 import polars as pl
@@ -12,44 +13,66 @@ from dotenv import load_dotenv
 from .env import get_env_or_raise, get_quiz_ids
 
 load_dotenv()
-grit_logger = logging.getLogger("grit")
-grit_logger.setLevel(logging.DEBUG)
-file_handle = logging.FileHandler(get_env_or_raise("LOG_FILE"))
-file_handle.setLevel(logging.DEBUG)
-stderr_handler = logging.StreamHandler()
-grit_logger.addHandler(file_handle)
-grit_logger.addHandler(stderr_handler)
 
 COURSE_ID = get_env_or_raise("CANVAS_COURSE_ID")
-QUIZ_IDS = get_quiz_ids()
+QUIZ_NAME_TO_IDS = get_quiz_ids()
+QUIZ_IDS_TO_NAME = {v: k for k, v in QUIZ_NAME_TO_IDS.items()}
 API_URL = get_env_or_raise("CANVAS_API_URL")
 API_KEY = get_env_or_raise("CANVAS_API_KEY")
 GRIT_URL = get_env_or_raise("GRIT_URL")
 GRIT_API_KEY = get_env_or_raise("GRIT_API_KEY")
 UPLOAD_TO_GRIT = get_env_or_raise("UPLOAD_TO_GRIT") == "true"
+LOG_FOLDER = Path(get_env_or_raise("LOG_FOLDER"))
+
+grit_logger = logging.getLogger("grit")
+program_start = datetime.now(timezone.utc)
+
+
+def setup_logging():
+    grit_logger.setLevel(logging.DEBUG)
+    timestamp = datetime.now().strftime("%Y_%m_%d")
+    filepath = LOG_FOLDER / f"{timestamp}.txt"
+    filepath.touch()
+    file_handle = logging.FileHandler(filepath)
+    file_handle.setLevel(logging.DEBUG)
+    stderr_handler = logging.StreamHandler()
+    grit_logger.addHandler(file_handle)
+    grit_logger.addHandler(stderr_handler)
+
+
+setup_logging()
 
 
 def get_quiz_results(
     course: Course,
-    quiz_name: str,
-    quiz_id: int,
-) -> pl.LazyFrame:
-    assignment = course.get_assignment(quiz_id)
-    submissions = assignment.get_submissions()
+) -> pl.DataFrame:
+    submissions = course.get_multiple_submissions(
+        assignment_ids=QUIZ_NAME_TO_IDS.values(),
+        submitted_since=program_start - timedelta(days=7),
+        student_ids=["all"],
+    )
 
-    first_names: list[str] = []
-    last_names: list[str] = []
-    cwids: list[str] = []
-    scores: list[float] = []
-    emails: list[str] = []
+    SCHEMA = {
+        "firstName": pl.Utf8,
+        "lastName": pl.Utf8,
+        "externalId": pl.Utf8,
+        "email": pl.Utf8,
+        **{quiz_name: pl.Utf8 for quiz_name in QUIZ_NAME_TO_IDS.keys()},
+    }
 
-    grit_logger.info("Getting submissions for %s", quiz_name)
+    df = pl.DataFrame(schema=SCHEMA)
 
     for sub in submissions:
         if sub.grade is None:
             continue
-        score = float(sub.grade.replace("%", ""))
-        student = course.get_user(sub.user_id)
+
+        grade = float(cast(str, sub.grade).replace("%", ""))
+        if grade < 90:
+            continue
+
+        quiz_name = QUIZ_IDS_TO_NAME[sub.assignment_id]
+        user_id = sub.user_id
+        student = course.get_user(user_id)
         last_name, first_name = cast(
             tuple[str, str], [n.strip() for n in student.sortable_name.split(",")]
         )
@@ -62,37 +85,36 @@ def get_quiz_results(
             cwid,
             first_name,
             last_name,
-            score,
+            grade,
             email,
         )
 
-        first_names.append(first_name)
-        last_names.append(last_name)
-        cwids.append(cwid)
-        scores.append(score)
-        emails.append(email)
-
-    is_passing = pl.col(quiz_name) >= 90
-    return (
-        pl.LazyFrame(
+        row = pl.DataFrame(
             {
-                "firstName": first_names,
-                "lastName": last_names,
-                "externalId": cwids,
-                "email": emails,
-                quiz_name: scores,
-            },
-            {
-                "firstName": pl.Utf8,
-                "lastName": pl.Utf8,
-                "externalId": pl.Utf8,
-                "email": pl.Utf8,
-                quiz_name: pl.Float32,
-            },
+                "firstName": first_name,
+                "lastName": last_name,
+                "externalId": cwid,
+                "email": email,
+                **{q: "x" if q == quiz_name else "" for q in QUIZ_NAME_TO_IDS.keys()},
+            }
         )
-        .filter(is_passing)
-        .with_columns(is_passing)
+        df = df.vstack(row)
+
+    gb = ["firstName", "lastName", "externalId", "email"]
+    quiz_cols = pl.all().exclude(gb)
+    df = df.group_by(gb).agg(quiz_cols.str.concat(""))
+    print(df)
+    df = df.with_columns(
+        [
+            pl.when(quiz_cols.str.lengths() == 0)
+            .then(pl.lit(None))
+            .otherwise(pl.lit("x"))
+            .keep_name()
+        ]
     )
+    print(df)
+
+    return df
 
 
 def upsert_grit(file: io.BytesIO) -> requests.Response:
@@ -116,35 +138,28 @@ def main():
     canvas = Canvas(API_URL, API_KEY)
     course = canvas.get_course(COURSE_ID)
 
-    for quiz_name, quiz_id in QUIZ_IDS.items():
-        results = get_quiz_results(
-            course,
-            quiz_name,
-            quiz_id,
-        ).with_columns(
-            pl.when(pl.col(quiz_name))
-            .then(pl.lit("x"))
-            .otherwise(pl.lit(None))
-            .alias(quiz_name),
-            pl.lit("x").alias("at:Any Time"),
-            active=pl.lit("x"),
-        )
+    df = get_quiz_results(course).with_columns(
+        pl.lit("x").alias("at:Any Time"),
+        active=pl.lit("x"),
+    )
 
-        results = results.collect()
-        # if no changes for a quiz, then skip sending to grit
-        if len(results) == 0:
-            continue
+    # if no changes for a quiz, then skip sending to grit
+    if len(df) == 0:
+        grit_logger.info("N changes")
+        time_elapsed = datetime.now() - start_time
+        grit_logger.info(f"Completed in {time_elapsed.total_seconds():.2f} seconds")
+        return
 
-        grit_csv = io.BytesIO()
-        results.write_csv(grit_csv)
+    grit_csv = io.BytesIO()
+    df.write_csv(grit_csv)
 
-        grit_logger.info(f"CSV to send:\n{grit_csv.getvalue().decode('utf-8')}")
+    grit_logger.info(f"CSV to send:\n{grit_csv.getvalue().decode('utf-8')}")
 
-        if UPLOAD_TO_GRIT:
-            response = upsert_grit(grit_csv)
-            if not response.ok:
-                grit_logger.warning("Request to Grit didn't return OK")
-                grit_logger.warning(response.text)
+    if UPLOAD_TO_GRIT:
+        response = upsert_grit(grit_csv)
+        if not response.ok:
+            grit_logger.warning("Request to Grit didn't return OK")
+            grit_logger.warning(response.text)
 
     time_elapsed = datetime.now() - start_time
     grit_logger.info(f"Completed in {time_elapsed.total_seconds():.2f} seconds")
