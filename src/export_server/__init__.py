ae01b0e83f18d547adedbbb4a6d00eb7085ccf5d
David Krauthamer <dkrautha@pm.me>
2024-01-31 11:58:16 -0500

    Change export server to send most recent backup

--- src/export_server/__init__.py
+++ src/export_server/__init__.py
@@ -1,9 +1,5 @@
-import io
-from datetime import datetime
 from pathlib import Path
 
-import polars as pl
-import requests
 from dotenv import load_dotenv
 from flask import Flask, send_file
 
@@ -13,31 +9,19 @@ app = Flask(__name__)
 
 
 load_dotenv()
-GRIT_URL = get_env_or_raise("GRIT_URL")
-GRIT_API_KEY = get_env_or_raise("GRIT_API_KEY")
 BACKUP_FOLDER = Path(get_env_or_raise("BACKUP_FOLDER"))
 
 
 @app.route("/grit_export", methods=["GET"])
 def grit_export():
-    session = requests.Session()
-    request = session.prepare_request(
-        requests.Request(
-            method="GET",
-            url=f"{GRIT_URL}/api/batch/user/export",
-            headers={"x-auth-token": GRIT_API_KEY},
-        )
+    most_recent_file = max(
+        BACKUP_FOLDER.iterdir(), key=lambda x: x.stat().st_ctime, default=None
     )
-    response = session.send(request)
-    as_excel = io.BytesIO(response.content)
-
-    timestamp = datetime.now().strftime("%Y_%m_%d_%H_%M")
-    filepath = BACKUP_FOLDER / f"grit_export_{timestamp}.csv"
-    filepath.touch()
-    pl.read_excel(as_excel).write_csv(filepath)
+    if most_recent_file is None:
+        return "No files found", 404
 
     return send_file(
-        filepath,
+        most_recent_file,
         mimetype="text/csv",
         as_attachment=True,
         download_name="grit_export.csv",
