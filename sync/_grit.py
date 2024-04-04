ef1b649cc423663f9c73bd7cd5f691fb42102314
David Krauthamer <dkrautha@pm.me>
2024-04-04 12:38:51 -0400

    Add grit module

---
+++ sync/_grit.py
@@ -0,0 +1,51 @@
+import io
+from typing import Self
+
+import polars as pl
+import requests
+
+
+class Grit:
+    _session: requests.Session
+    _api_key: str
+    _url: str
+
+    def __init__(self: Self, url: str, api_key: str) -> None:
+        self._session = requests.Session()
+        self._api_key = api_key
+        self._url = url
+
+    def upsert(self: Self, df: pl.DataFrame) -> requests.Response:
+        # TODO(David): might be able to get away with just using write_csv returning a string, but unsure if requests will be happy with that
+        # leaving as is for now to not break anything
+        csv = io.BytesIO()
+        df.write_csv(csv)
+
+        request = self._session.prepare_request(
+            requests.Request(
+                method="POST",
+                url=f"{self._url}/api/batch/user/upsert",
+                # url=f"{GRIT_URL}/api/batch/user/upsert?",
+                # "processPermissionGroups=true&"
+                # "processRFiDCards=false&"
+                # "processDemographics=false&"
+                # "processAccessTimes=true&"
+                # "processMobileGritCard=false",
+                headers={"x-auth-token": self._api_key},
+                files={"file": ("upload.csv", csv.getvalue(), "text/csv")},
+            ),
+        )
+
+        return self._session.send(request)
+
+    def get_backup(self: Self) -> pl.DataFrame:
+        request = self._session.prepare_request(
+            requests.Request(
+                method="GET",
+                url=f"{self._url}/api/batch/user/export",
+                headers={"x-auth-token": self._api_key},
+            ),
+        )
+
+        response = self._session.send(request)
+        return pl.read_excel(response.content)
