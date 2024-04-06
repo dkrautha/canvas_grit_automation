a872e97b76f9fd594bfd7246d24139ce343856ef
David Krauthamer <dkrautha@pm.me>
2024-04-06 16:32:25 -0400

    Add json support for using extra kwargs in invocation

--- jsonl_formatter/__init__.py
+++ jsonl_formatter/__init__.py
@@ -1,4 +1,8 @@
-""""""
+"""A logging formatter that outputs logs in jsonl format.
+
+Credit goes to James Murphy of mCoding, this implementation is heavily based on his work.
+https://github.com/mCodingLLC/VideosSampleCode/blob/master/videos/135_modern_logging/mylogger.py
+"""
 
 from __future__ import annotations
 
@@ -6,6 +10,32 @@ import datetime as dt
 import json
 import logging
 from typing import Self, override
+
+LOG_RECORD_BUILTIN_ATTRS = {
+    "args",
+    "asctime",
+    "created",
+    "exc_info",
+    "exc_text",
+    "filename",
+    "funcName",
+    "levelname",
+    "levelno",
+    "lineno",
+    "module",
+    "msecs",
+    "message",
+    "msg",
+    "name",
+    "pathname",
+    "process",
+    "processName",
+    "relativeCreated",
+    "stack_info",
+    "thread",
+    "threadName",
+    "taskName",
+}
 
 
 class JsonlFormatter(logging.Formatter):
@@ -46,5 +76,9 @@ class JsonlFormatter(logging.Formatter):
             for key, val in self.fmt_keys.items()
         }
         message.update(always_fields)
+
+        for key, val in record.__dict__.items():
+            if key not in LOG_RECORD_BUILTIN_ATTRS:
+                message[key] = val
 
         return message
